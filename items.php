8774a7e3c4f57848f7e6b5d70b3b44af70b0ef37
Filipe Correia <68955432+fmcorreia04@users.noreply.github.com>
2024-04-08 15:57:53 +0100

    implemented items

--- items.php
+++ items.php
@@ -1,14 +1,17 @@
 <?php 
     require_once('templates/common.php');
+    require_once('db/connection.php');
     require_once('templates/home.php');
+    require_once('db/products.php');
 
     output_header("Our catalogue", null);
 ?>
-
 <main>
 
 <?php
-    $items = 
+    $db = getDatabaseConnection();
+    $items = getAllProducts($db);
+    output_list_items($items);
 ?>
 
 </main>
