537275a0a6575eeee0654eb0bc866991979ae034
g0ncal0 <goncalonunes1112@gmail.com>
2024-04-08 15:31:38 +0100

    Functions to print products now use database

--- templates/items.php
+++ templates/items.php
@@ -1,20 +1,20 @@
 <?php 
-    function output_item() { ?>
+    function output_item($product) { ?>
         <div class="box-item">             
             <img src="img/dress.jpeg">
         
             <div class="box-details">
-                <h3>Title</h3>
-                <p class="info"><span>NEW</span><span>BRAND</span></p>
+                <h3><?php echo $product['name']?></h3>
+                <p class="info"><span>NEW</span><span><?php echo $product['Brand']?></span></p>
                 <div>
-                    <p>PRICE</p>
+                    <p><?php echo $product['price']?></p>
                     <button class="button">ADD TO CART</button>
                 </div>
             </div>
         </div>
     <?php }
 
-    function output_full_item() { ?>
+    function output_full_item($product) { ?>
         <section class="item-page">
             <div class="item-page-photos">
                 <img src="img/dress.jpeg">                
@@ -25,11 +25,11 @@
             </div>
         
             <div class="container">
-                <h1>Title</h1>
-                <p class="info"><span>NEW</span><span>BRAND</span><span>TAMANHO</span></p>
-                <p>Uma pequena descrição sobre o produto</p>
+                <h1><?php echo $product['name']?></h1>
+                <p class="info"><span>NEW</span><span><?php echo $product['Brand']?></span><span><?php echo $product['size']?></span></p>
+                <p><?php echo $product['description']?></p>
     
-                <p>PRICE</p>
+                <p><?php echo $product['price']?></p>
                 <button class="button">FAVORITES</button>
                 <button class="button">ADD TO CART</button>
                 <button class="button">ASK USER</button>
@@ -37,32 +37,30 @@
         </section>
     <?php }
 
-    function output_cart_item() { ?>
+    function output_cart_item($product) { ?>
         <div class="box-item">             
             <img src="img/dress.jpeg">
         
             <div class="box-details">
-                <h3>Title</h3>
+                <h3><?php echo $product['name']?></h3>
                 <div>
-                    <p>PRICE</p>
+                    <p><?php echo $product['name']?></p>
                 </div>
             </div>
         </div>
     <?php }
 
-    function output_list_items() {
-        // Esta função é para receber lista de items, está provisório assim
-        output_item();
-        output_item();
-        output_item();
-        output_item();
-    }
+    function output_list_items($products) {?>
+        <section class="products">
+            <h2>Products</h2>
+            <?php foreach($products as $product) output_item($product); ?>
+        </section>
+    <?php }
 
-    function output_list_cart_items() {
-        // Esta função é para receber lista de items, está provisório assim ?>
-        <h2>Cart</h2> <?php
-        output_cart_item();
-        output_cart_item();
-        output_cart_item();
-    }
+    function output_list_cart_items($products) { ?>
+        <section class="cart">
+            <h2>Cart</h2>
+            <?php foreach($products as $product) output_cart_item($product); ?>
+        </section>
+    <?php }
 ?>
