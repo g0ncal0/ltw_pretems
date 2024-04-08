e88d60dac4980cb4cc06c2264ec3eaadd90de05f
Filipe Correia <68955432+fmcorreia04@users.noreply.github.com>
2024-04-08 14:51:23 +0100

    Database v1

--- db/create.sql
+++ db/create.sql
@@ -4,29 +4,45 @@
 CREATE TABLE users 
 {
     name TEXT NOT NULL,
-    ID INTEGER NOT NULL,
+    id INTEGER NOT NULL PRIMARY KEY,
     email TEXT  NOT NULL,
     username TEXT NOT NULL,
     password TEXT NOT NULL,
-    admin BOOLEAN NOT NULL
+    admin BOOLEAN
 }
 
 CREATE TABLE categories{
-    ID INTEGER NOT NULL,
+    id INTEGER NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
 }
 
+CREATE TABLE brands{
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+}
+
+CREATE TABLE condition{
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL
+}
+
+CREATE TABLE productimgs (
+    productid INTEGER REFERENCES products,
+    filename TEXT NOT NULL,
+);
+
+/* TODO: check if size is xs ... xxl (with PHP) */
 CREATE TABLE products
 {
-    date DATE NOT NULL,
-    category 
-    brand
-    model
-    size
-    condition
-    price
-    user
-    images
-    available
+    date DATETIME NOT NULL,
+    category INTEGER NOT NULL REFERENCES categories,
+    brand INTEGER REFERENCES brands,
+    model TEXT NOT NULL,
+    size TEXT NOT NULL, 
+    condition INTEGER REFERENCES condition,
+    price REAL NOT NULL,
+    user INTEGER NOT NULL REFERENCES users, 
+    available BOOLEAN NOT NULL,
 }
 
