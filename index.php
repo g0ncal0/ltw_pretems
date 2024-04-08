bce4b8c77b351c8ef1b1ceb983f90f61a55d502a
Filipe Correia <68955432+fmcorreia04@users.noreply.github.com>
2024-04-08 15:34:24 +0100

    mini refactor

--- index.php
+++ index.php
@@ -2,7 +2,24 @@
     require_once('templates/common.php');
     require_once('templates/home.php');
 
-    output_header();
-    output_home_page();
+    output_header(null, null);
+?>
+<main>
+    <section class="box home-box">
+        <div>
+            <h1>PRETEMS</h1>
+            <p>The website where you can find preloved items with quality</p>
+            <p>Pre-loved items you will adore at a price that will blow you away..</p>
+        </div>
+        <img src="">
+    </section>
+
+    <?php output_featured() ?>
+
+    <?php output_categories() ?>
+
+</main>
+
+<?php
     output_footer();
 ?>
---
+++ items.php
@@ -0,0 +1,15 @@
+<?php 
+    require_once('templates/common.php');
+    require_once('templates/home.php');
+
+    output_header("Our catalogue", null);
+?>
+
+<main>
+
+<?php
+    $items = 
+?>
+
+</main>
+
