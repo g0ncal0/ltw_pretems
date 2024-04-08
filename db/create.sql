17de93b2519734526828ed4a6f8e97479167ecfe
Filipe Correia <68955432+fmcorreia04@users.noreply.github.com>
2024-04-08 14:57:52 +0100

    Database v1.1 (working with minor syntax bugs corrected)

--- db/create.sql
+++ db/create.sql
@@ -1,40 +1,45 @@
 
-
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
+DROP tABLE IF EXISTS brands;
+DROP TABLE IF EXISTS condition;
+DROP TABLE IF EXISTS productimgs;
+DROP TABLE IF EXISTS products;
 
 CREATE TABLE users 
-{
+(
     name TEXT NOT NULL,
     id INTEGER NOT NULL PRIMARY KEY,
     email TEXT  NOT NULL,
     username TEXT NOT NULL,
     password TEXT NOT NULL,
     admin BOOLEAN
-}
+);
 
-CREATE TABLE categories{
+CREATE TABLE categories(
     id INTEGER NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL,
-}
+    name TEXT NOT NULL
+);
 
-CREATE TABLE brands{
+CREATE TABLE brands(
     id INTEGER PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
-}
+    name TEXT NOT NULL
+);
 
-CREATE TABLE condition{
+CREATE TABLE condition(
     id INTEGER PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
     description TEXT NOT NULL
-}
+);
 
 CREATE TABLE productimgs (
     productid INTEGER REFERENCES products,
-    filename TEXT NOT NULL,
+    filename TEXT NOT NULL
 );
 
 /* TODO: check if size is xs ... xxl (with PHP) */
 CREATE TABLE products
-{
+(
     date DATETIME NOT NULL,
     category INTEGER NOT NULL REFERENCES categories,
     brand INTEGER REFERENCES brands,
@@ -43,6 +48,6 @@ CREATE TABLE products
     condition INTEGER REFERENCES condition,
     price REAL NOT NULL,
     user INTEGER NOT NULL REFERENCES users, 
-    available BOOLEAN NOT NULL,
-}
+    available BOOLEAN NOT NULL
+);
 
