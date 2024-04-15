b8a36e555f19a8194b5a404a85ca63e65fa506cb
vanessa-sbq <up202207919@up.pt>
2024-04-15 18:42:00 +0100

    Initial register code

--- register.php
+++ register.php
@@ -14,14 +14,14 @@
         <h1>Register a new account</h1>
         <form class="account-form" action="actions/action_register.php" method="post">
             <label for="name">Name</label>
-            <input type="text" name="name" id="name">
+            <input type="text" name="r-name" id="r-name">
 
 
             <label for="email">Email</label>
-            <input type="email" name="email" id="email">
+            <input type="email" name="r-email" id="r-email">
 
             <label for="password">Password</label>
-            <input type="password" name="password" id="password">
+            <input type="password" name="r-password" id="r-password">
 
             <button class="button" type="submit">Register</button>
         </form>
