e70d985cad422757ae0e605998ce00994cc7221c
vanessa-sbq <up202207919@up.pt>
2024-04-27 20:17:51 +0100

    Added CSS to part of admin area

--- admin_area.php
+++ admin_area.php
@@ -11,17 +11,23 @@
     protectPage($session);
 
     if ($_GET['area'] == 'category'){
-        output_header($db, 'Add Category', null, $session->getId()); 
+        output_header($db, 'Add Category', null, $session->getId());
+        ?><section class="container"> 
+            <h1>Add Category:</h1><?php
         output_category_form();
         output_existing_categories();
     }
     else if ($_GET['area'] == 'size'){
         output_header($db, 'Add Size', null, $session->getId()); 
+        ?><section class="container"> 
+            <h1>Add Size:</h1><?php
         output_size_form();
         output_existing_sizes();
     }
     else if ($_GET['area'] == 'condition'){
-        output_header($db, 'Add Condition', null, $session->getId()); 
+        output_header($db, 'Add Condition', null, $session->getId());
+        ?><section class="container"> 
+            <h1>Add Condition:</h1><?php 
         output_condition_form();
         output_existing_conditions();
     }
@@ -29,6 +35,7 @@
     if (isset($_GET['message'])) {
         ?><p><?php echo $_GET['message']?></p><?php // Print if it was added successfully
     } 
+    ?></section><?php
 
     output_footer(); 
 ?>
