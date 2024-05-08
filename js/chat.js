adcb6c290ea2c1e1980d0960dddf11f49ed00d8f
vanessa-sbq <up202207919@up.pt>
2024-05-08 15:12:34 +0100

    Added styles to chat

--- js/chat.js
+++ js/chat.js
@@ -30,27 +30,38 @@ if (newMessageButton) {
         const message = await response.json()
 
         const newDiv = document.createElement("div");
+        const newDivMsg = document.createElement("div");
         
         let user = null;
+        let userPfp = null;
 
         if (message.fromBuyer) {
             user = await getUserName(buyerId.value);
-            newDiv.className = "messageFromBuyer";
+            userPfp = await getUserPfp(buyerId.value);
+            newDiv.className = "fromBuyer";
+            newDivMsg.className = "messageFromBuyer";
         }
         else {
             const product = await getProduct(productId.value);
             user = await getUserName(product.user);
-            newDiv.className = "messageFromSeller";
+            userPfp = await getUserPfp(product.user);
+            newDiv.className = "fromSeller";
+            newDivMsg.className = "messageFromSeller";
         }
 
-        newDiv.innerHTML = `
-            <p>${message.message}</p>
-            <footer>
-                ${user} ${message.date}
-            </footer>
+        newDiv.innerHTML = `            
+            <img src="${userPfp}" alt="Profile Image" class="profile-image">        
         `;
 
-        const section = document.querySelector('.messages')
+        newDivMsg.innerHTML = `
+            <p>${message.message}</p>
+            <p class='message-from'>
+                ${user} ${message.date}
+            </p>
+        `;
+
+        const section = document.querySelector('.messages');
+        newDiv.appendChild(newDivMsg);
         section.appendChild(newDiv);
 
         newMessage.value = ''
@@ -67,4 +78,10 @@ async function getProduct(productId) {
     const response = await fetch(`/api/getProduct.php?productId=${productId}`);
     const data = await response.json();
     return data;
+}
+
+async function getUserPfp(userId){
+    const response = await fetch(`/api/getUser.php?userId=${userId}`);
+    const data = await response.json();
+    return data.profileImg;
 }
