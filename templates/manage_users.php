6bf1e5c13c9d59d98aedbaca79970459b8973614
vanessa-sbq <up202207919@up.pt>
2024-05-09 22:05:26 +0100

    Small fix in Manage Users design

--- templates/manage_users.php
+++ templates/manage_users.php
@@ -7,15 +7,17 @@
                 foreach ($users as $user) { ?> 
                     <div class="user">
                         <img src="<?= $user['profileImg'] ?>" alt="Profile Image" class="profile-image">
-                        <p class="user-name">Name: <?= $user['name'] ?></p>
-                        <p class="user-email">Email: <?= $user['email'] ?></p>
-                        <p class="user-role">Role: 
-                            <?php if (!$user['admin']) { ?>
-                                Not an admin <a href="/actions/action_elevate_admin.php?id=<?= $user['id'] ?>">Elevate to admin</a>
-                            <?php } else { ?>
-                                Admin
-                            <?php } ?>
-                        </p>
+                        <div class="manage-user-info">
+                            <p class="user-name">Name: <?= $user['name'] ?></p>
+                            <p class="user-email">Email: <?= $user['email'] ?></p>
+                            <p class="user-role">Role: 
+                                <?php if (!$user['admin']) { ?>
+                                    Not an admin <a href="/actions/action_elevate_admin.php?id=<?= $user['id'] ?>">Elevate to admin</a>
+                                <?php } else { ?>
+                                    Admin
+                                <?php } ?>
+                            </p>
+                        </div>
                     </div><?php
                 }
             ?></div>
